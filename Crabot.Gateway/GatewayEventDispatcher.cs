6891df76ee1c33828ff9a60275c84b417c84e89e
fonter <serfin009@gmail.com>
2020-12-28 23:52:28 +0100

    added test external logging

--- Crabot.Gateway/GatewayEventDispatcher.cs
+++ Crabot.Gateway/GatewayEventDispatcher.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Crabot.Commands;
 using Crabot.Contracts;
 using Crabot.Core.Events;
+using Crabot.Rest.RestClient;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -15,15 +16,18 @@ namespace Crabot.Gateway
 
         private readonly ILogger _logger;
         private readonly ICommandProcessor _commandProcessor;
+        private readonly IDiscordRestClient _discordRestClient;
         private readonly IServiceProvider _serviceProvider;
 
         public GatewayEventDispatcher(
             ILogger<GatewayEventDispatcher> logger, 
             ICommandProcessor commandProcessor, 
+            IDiscordRestClient discordRestClient,
             IServiceProvider serviceProvider)
         {
             _logger = logger;
             _commandProcessor = commandProcessor;
+            _discordRestClient = discordRestClient;
             _serviceProvider = serviceProvider;
         }
 
@@ -71,14 +75,18 @@ namespace Crabot.Gateway
                 case GatewayOpCode.Hello:
                     await _serviceProvider.GetRequiredService<IGatewayEventHandler<HeartbeatEvent>>()
                             .HandleAsync(@event.EventData);
+                    await _discordRestClient.PostMessage("764840399696822322", "Heartbeat interval - " + @event.EventData.ToString());
                     break;
                 case GatewayOpCode.Reconnect:
                     _logger.LogWarning("Server requesting reconnect");
+                    await _discordRestClient.PostMessage("764840399696822322", "Server requesting reconnect");
                     await _serviceProvider.GetRequiredService<IGatewayEventHandler<ReconnectEvent>>()
                             .HandleAsync(@event.EventData);
+                    await _discordRestClient.PostMessage("764840399696822322", "Successfully resumed session!");
                     break;
                 case GatewayOpCode.InvalidSession:
-                    _logger.LogWarning("Cannot resume session! Starting new session!");
+                    _logger.LogWarning("Cannot resume session!");
+                    await _discordRestClient.PostMessage("764840399696822322", "Cannot resume session!");
                     break;
                 case GatewayOpCode.HeartbeatAck:
                     _logger.LogInformation("Session prolongate successful!");
