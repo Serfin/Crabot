fc766bd1f30a5f056577a9e2744056b5728e5dd1
fonter <serfin009@gmail.com>
2021-04-02 23:46:14 +0200

    added basic unit tests for command validation

---
+++ Crabot.Tests/CommandValidatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Crabot.Commands.Commands;
+using Crabot.Contracts;
+using Crabot.Core.Repositories;
+using Moq;
+using NUnit.Framework;
+
+namespace Crabot.Tests
+{
+    [TestFixture]
+    public class CommandValidatorTests
+    {
+        private Mock<IClientInfoRepository> _clientInfoReposotoryMock;
+        private CommandValidator _commandValidator;
+
+        private string crabotFakeAuthorId = "112233445566778899";
+
+        [SetUp]
+        public void Setup()
+        {
+            _clientInfoReposotoryMock = new Mock<IClientInfoRepository>();
+            _clientInfoReposotoryMock
+                .Setup(x => x.GetClientInfo())
+                .Returns(new ClientInfo
+                {
+                    User = new User { Id = crabotFakeAuthorId }
+                });
+
+            _commandValidator = new CommandValidator(_clientInfoReposotoryMock.Object);
+        }
+
+        [Test]
+        public void Command_with_prefix_is_valid_command()
+        {
+            var testCommand = new GatewayMessage 
+            { 
+                Content = "?test-command", 
+                Author = new Author 
+                { 
+                    Id = It.IsAny<string>() 
+                } 
+            };
+
+            var validationResult = _commandValidator.IsCommand(testCommand);
+
+            Assert.True(validationResult);
+        }
+
+        [Test]
+        public void Bot_cannot_handle_its_own_commands()
+        {
+            var testCommand = new GatewayMessage
+            {
+                Content = "?test-command",
+                Author = new Author
+                {
+                    Id = crabotFakeAuthorId
+                }
+            };
+
+            var validationResult = _commandValidator.IsCommand(testCommand);
+
+            Assert.False(validationResult);
+        }
+    }
+}
