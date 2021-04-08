c2f92cc8b3630ca1f9db25290dc94d569f4d8025
fonter <serfin009@gmail.com>
2021-04-08 02:08:33 +0200

    changed ps scripts

--- Crabot/start-in-background.ps1
+++ Crabot/start-in-background.ps1
@@ -1,3 +1,5 @@
+Write-Host "Starting process 'Crabot'"
 $Path = ".\bin\Debug\netcoreapp3.1\Crabot.exe"
-
 Start-Process $Path -WindowStyle Hidden
+
+Read-Host
--- Crabot/stop-application.ps1
+++ Crabot/stop-application.ps1
@@ -1,5 +1,9 @@
 $Crabot_process = Get-Process -Name Crabot
 
-if ($Crabot_process.Id -ne $null) {
+if ($Crabot_process -ne $null && $Crabot_process.Id -ne $null) {
+	Write-Output "Stopping process 'Crabot' with PID"
+	Write-Output $Crabot_process.Id
 	Stop-Process $Crabot_process.Id
 }
+
+Read-Host
