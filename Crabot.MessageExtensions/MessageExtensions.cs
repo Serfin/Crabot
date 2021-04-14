cf1b3f173442e75f9e4614658675ec884c31e853
fonter <serfin009@gmail.com>
2021-04-15 00:37:32 +0200

    added message extension utilities

---
+++ Crabot.MessageExtensions/MessageExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Crabot.Contracts;
+
+namespace Crabot.MessageExtensions
+{
+    public static class MessageUtilities
+    {
+        public static string MentionUser(string userId)
+        {
+            return $"<@{userId}>";
+        }
+
+        public static string UseEmoji(Emoji emoji)
+        {
+            return $"<:{emoji.Name}:{emoji.Id}>";
+        }
+    }
+}
